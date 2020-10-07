2ed6b0c73020e99d0ca16cac83ccd47820924528
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-10-07 09:13:15 +0000

    finish kadai6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #直前のurlを取得するメソッド
-    redirect_to request.referer
+    #redirect_to request.referer
+    #Ajax
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
+    #Ajax
   end
 
 end
